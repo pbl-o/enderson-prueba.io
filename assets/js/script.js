d00174180ea816cccaafa40b3c1d20dafeefdd92
Enderson <endersonamaya@gmail.com>
2022-05-13 20:26:23 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// tooltips Iconos redes//
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Popover boton enviar //
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// Funcionamiento navbar //
+$(document).scroll( function(e){
+  const y = $("html").scrollTop();
+  if(y > 1300) $("nav").addClass("nav-black")
+  else $("nav").removeClass("nav-black")
+})
