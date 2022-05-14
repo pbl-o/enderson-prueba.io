335159d1001615eb2bd41bd43a527d36b6e75ca9
Enderson <endersonamaya@gmail.com>
2022-05-13 20:40:48 -0400

    cuarto commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ $(document).scroll( function(e){
   else $("nav").removeClass("nav-black")
 })
 
+// Popover boton enviar //
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
 // tooltips Iconos redes//
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
@@ -12,8 +18,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-// Popover boton enviar //
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
